08a1a7d576761368ccec88fb7fa76fa79c3a295d
ningenMe <amohkuru@gmail.com>
2024-05-13 03:30:26 +0900

    [fix]

--- geeq-frontend/app/page.tsx
+++ geeq-frontend/app/page.tsx
@@ -7,8 +7,8 @@ import { GITHUB_CLIENT_ID } from "../components/constant";
 
 export default function Page() {
   // TODO local/本番で使い分ける
-  const redirect_url = process.env.NEXT_PUBLIC_FRONT_ORIGIN + "/auth/callback"
-  const github_oauth_url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_url=${redirect_url}`;
+  const redirect_uri = process.env.NEXT_PUBLIC_FRONT_ORIGIN + "/auth/callback"
+  const github_oauth_url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${redirect_uri}`;
   const [loginUserId, setLoginUserId] = useState<string | null>(null);
   const router = useRouter();
 
@@ -34,7 +34,7 @@ export default function Page() {
         LOGOUT
       </button>
       <h1>Hello, Geeq</h1>
-      <h1>redirect_url = {redirect_url}</h1>
+      <h1>redirect_uri = {redirect_uri}</h1>
       <a>{loginUserId ? loginUserId+" Logined" : "You Are Guest User"}</a>
     </>
   )
