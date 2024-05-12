6941d0192b7fb3593046eaa5757fedad4d025879
ningenMe <amohkuru@gmail.com>
2024-05-13 02:24:49 +0900

    [fix]

--- geeq-frontend/app/page.tsx
+++ geeq-frontend/app/page.tsx
@@ -1,16 +1,14 @@
 "use client"
 
-import { NextPage } from "next";
 import { geeqApiClient } from "../components/client/GeeqApiClient";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { GITHUB_CLIENT_ID } from "../components/constant";
 
 export default function Page() {
-  // TODO 環境変数に入れる
-  const github_client_id = "Iv1.17c5ebb1ad2d2832";
   // TODO local/本番で使い分ける
-  const redirect_url = "http://localhost:3000/auth/callback"
-  const github_oauth_url = `https://github.com/login/oauth/authorize?client_id=${github_client_id}&redirect_url=${redirect_url}`;
+  const redirect_url = process.env.NEXT_PUBLIC_FRONT_ORIGIN + "/auth/callback"
+  const github_oauth_url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_url=${redirect_url}`;
   const [loginUserId, setLoginUserId] = useState<string | null>(null);
   const router = useRouter();
 
@@ -36,6 +34,7 @@ export default function Page() {
         LOGOUT
       </button>
       <h1>Hello, Geeq</h1>
+      <h1>redirect_url = {redirect_url}</h1>
       <a>{loginUserId ? loginUserId+" Logined" : "You Are Guest User"}</a>
     </>
   )
---
+++ geeq-frontend/components/constant.ts
@@ -0,0 +1,1 @@
+export const GITHUB_CLIENT_ID = "Iv1.17c5ebb1ad2d2832";
