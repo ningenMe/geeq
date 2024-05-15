26f1b580b9bd7c919322ac00444f8e5b8425c536
ningenMe <amohkuru@gmail.com>
2024-05-16 02:56:24 +0900

    [fix]

--- geeq-frontend/app/page.tsx
+++ geeq-frontend/app/page.tsx
@@ -1,29 +1,12 @@
-"use client";
-
-import { geeqApiClient } from "../components/client/GeeqApiClient";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { GITHUB_CLIENT_ID } from "../components/constant";
+import { LoginButton, LogoutButton, Me } from "../components/root";
 
 export default function Page() {
-  // TODO local/本番で使い分ける
-  const redirect_uri = process.env.NEXT_PUBLIC_FRONT_ORIGIN + "/auth/callback";
-  const github_oauth_url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${redirect_uri}`;
-  const [loginUserId, setLoginUserId] = useState<string | null>(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    geeqApiClient.authMeGet({ withCredentials: true }).then((res) => {
-      setLoginUserId(res.data.userId);
-    });
-  }, [geeqApiClient]);
   return (
     <>
       <LoginButton />
       <LogoutButton />
       <h1>Hello, Geeq</h1>
-      <h1>redirect_uri = {redirect_uri}</h1>
-      <a>{loginUserId ? loginUserId + " Logined" : "You Are Guest User"}</a>
+      <Me />
     </>
   );
 }
